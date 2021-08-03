2febaf20a4f178e2b1c8ecd58ffa7b21b8257ea2
Rafael Tsuyoshi Yamashita <ryamashita@daitan.com>
2021-08-03 09:53:05 -0300

    start app

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,35 @@
+const db = require("../models")
+const Tutorial = db.tutorials
+const Op = db.Sequelize.Op
+
+// criar e salvar tutorial
+exports.create = (req, res) => {
+
+}
+
+exports.findall = (req, res) => {
+
+}
+
+
+exports.findOne = (req, res) => {
+    
+}
+
+
+exports.update = (req, res) => {
+    
+}
+
+exports.delete = (req, res) => {
+    
+}
+
+exports.deleteAll = (req, res) => {
+    
+}
+
+exports.findAllPublished = (req, res) => {
+    
+}
+
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+}
+
+// seta origin para http://localhost:8081
+app.use(cors(corsOptions))
+
+// utilizado para transformar dados em json
+app.use(express.json())
+
+// utilizado para transformar dados em string ou arrays
+// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+
+const db = require("./app/models")
+
+db.sequelize.sync()
+
+// Deletar e sincronizar com o banco de dados
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+// });
+
+// rota simples
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to the application."})
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
